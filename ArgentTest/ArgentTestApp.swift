bfb835ba66e0052e1fc661b31035c030edc81f5b
Alex little <alex@reelapp.co.uk>
2021-10-29 16:15:16 +0100

    Initial Commit

---
+++ ArgentTest/ArgentTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgentTestApp.swift
+//  ArgentTest
+//
+//  Created by Alex Little on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ArgentTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
